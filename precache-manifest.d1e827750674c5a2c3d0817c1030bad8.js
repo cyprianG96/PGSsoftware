67e9ebdc442cfa4b49e85ad7fd2014936d3f6c3a
Cyprian <cyprian.g55@gmail.com>
2019-04-17 21:52:10 +0200

    Updates

---
+++ precache-manifest.d1e827750674c5a2c3d0817c1030bad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9754f899089f12f9e2811c4ffe71410f",
+    "url": "/PGSsoftware/static/media/logo.9754f899.jpg"
+  },
+  {
+    "revision": "0e4053e6c5c2d926b691",
+    "url": "/PGSsoftware/static/js/runtime~main.8af134c5.js"
+  },
+  {
+    "revision": "437c33045ced749fc029",
+    "url": "/PGSsoftware/static/js/main.0d536ed1.chunk.js"
+  },
+  {
+    "revision": "d992802d6fd415720231",
+    "url": "/PGSsoftware/static/js/2.c6e6450b.chunk.js"
+  },
+  {
+    "revision": "437c33045ced749fc029",
+    "url": "/PGSsoftware/static/css/main.3b651028.chunk.css"
+  },
+  {
+    "revision": "9143033a7bd7d192cf367f42152b89e1",
+    "url": "/PGSsoftware/index.html"
+  }
+];
